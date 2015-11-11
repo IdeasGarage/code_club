fd2195573e9fdd6909b37ded90709f4e45a8cf8b
coder <coder@Monmouthshires-MacBook-Air-4.local>
2015-11-11 16:56:48 +0000

    robot_arm.movement

--- Robot_Arm/arm_alfie.py
+++ Robot_Arm/arm_alfie.py
@@ -1,14 +1,12 @@
 import sys
-sys.path = [ ‘ . /lib’ ] + sys.path
+sys.path = [ './lib' ] + sys.path
 
 from robot_arm import  *
 
 arm = robot_arm ( )
 
-arm.move_arm_timed(WRIST_DOWN,4)
-arm.move_arm_timed(GRIP_CLOSE,2)                                                              
-arm.move_arm_timed(ELBOW_DOWN,3)
-arm.move_arm_timed(SHOULDER_UP,1)
-arm.move_arm_timed(BASE_COUNTERCLOCKWISE,2)
-arm.move_arm_timed(BASE_CLOCKWISE,2)
-                   
+arm.move_arm_timed(ELBOW_UP,4)
+arm.move_arm_timed(GRIP_OPEN,2)                                                              
+arm.move_arm_timed(BASE_CLOCKWISE,1)
+arm.move_arm_timed(SHOULDER_DOWN,3)
+arm.move_arm_timed(WRIST_UP,1)
