91e01664c01b02bd8bcfd812c2ccdfcdb9e8546a
Your Name <you@example.com>
2016-11-02 16:59:56 +0000

    hello

--- gpio/led.py
+++ gpio/led.py
@@ -1,5 +1,5 @@
-import RPi.GPIO as GPIO ## Import GPIO library
-GPIO.setmode(GPIO.BOARD) ## Use board pin numbering
-GPIO.setup(7, GPIO.OUT) ## Setup GPIO Pin 7 to OUT
-GPIO.output(7,True) ## Turn on GPIO pin 7
+import RPi.GPIO as GPIO 
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(7, GPIO.OUT) 
+GPIO.output(7,True) 
 raw_input()
